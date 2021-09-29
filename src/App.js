315bd808f601b55f02afeb5466947c81790c509d
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2021-09-29 16:31:26 +0100

    Splitting out components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import React  from 'react';
+import { useState } from 'react';
 import './App.css';
 
+const ButtonNumber = props => (
+  <button 
+    className="number"
+    onClick={() => console.log('Num', props.number)}
+    >
+      {props.number}
+  </button>
+)
+
+const StarElement = props => (
+  utils.range(1, props.count).map(starId => 
+    <div key={starId} className="star"/>
+  )
+)
+
 const StarMatch = () => {
+  const [stars, appendStars] = useState(utils.random(1, 9));
+
   return (
     <div className="game">
       <div className="help">
@@ -8,26 +27,12 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
+          <StarElement count={stars}/>
         </div>
         <div className="right">
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+        {utils.range(1, 9).map(number => 
+            <ButtonNumber key={number} number={number} />
+          )}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
